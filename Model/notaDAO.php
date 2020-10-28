04e0ac40791825c5612c5ee5794d56ff5ebf9411
Victormg42 <100005779.joan23@fje.edu>
2020-10-28 16:59:13 +0100

    Actualizacion

--- Model/notaDAO.php
+++ Model/notaDAO.php
@@ -8,6 +8,13 @@ class NotaDao{
     }
 
     public function modificarNotas(){
-        
+        $query = "UPDATE `tbl_notas` 
+        INNER JOIN `tbl_alumnos` 
+        ON tbl_alumnos.id_alum=tbl_notas.id_alum 
+        SET tbl_notas.`nom_asig`= 'Matematicas', tbl_notas.`nota`= '8' 
+        WHERE tbl_notas.nom_asig AND nota IS NULL AND tbl_alumnos.`id_alum` = ?;";
+        $sentencia3 = $pdo->prepare($query);
+        $sentencia3->execute();
+        $pdo->commit();
     }
 }
